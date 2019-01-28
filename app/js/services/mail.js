81c829494ddede5a3c16c979984bc534975692d5
Rafael Lucas de Campos <rafa.luck46@gmail.com>
2019-01-28 03:45:08 -0200

    Correção:  - Email  - Page footer sem a parte azul clara

--- app/js/services/mail.js
+++ app/js/services/mail.js
@@ -1,19 +1,6 @@
 var baseUrl = 'http://localhost:5000/mail';//'/mail';
 
-var DIRECT_TO = {
-    PARADOXY: 1,
-    NW_SOCIAL_ARTS: 2
-}
-
-Object.freeze(DIRECT_TO);
-
 export function sendMail(data) {
-
-    switch (data.directTo) {
-        case DIRECT_TO.PARADOXY: baseUrl = 'http://localhost:5000/mail'; //'https://paradoxy.com.br/mail'
-            break;
-    }
-debugger;
     return fetch(`${baseUrl}/sendmail`,
         {
             method: 'POST',
